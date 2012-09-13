13fbabe9240667d7f367b8bd0bc3f3e57fcea605
Josh Douglass-Molloy <joshuadm@gmail.com>
2012-09-13 16:10:15 +0930

    main tidied, model needs map generation, planets need position and assignment of id and name would be cool

--- model.py
+++ model.py
@@ -7,5 +7,9 @@ import buildings
 class Model:
 
     def __init__(self, eventmanager):
+        self.event = eventmanager
+        self.make_space()
+
+    #Generate initial game model
+    def make_space(self):
         pass
-    
