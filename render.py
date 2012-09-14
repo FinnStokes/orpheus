441e0a7bba42899f3532ee28b91b93e1462a2391
Finn Stokes <finn.stokes@gmail.com>
2012-09-14 15:03:05 +0930

    Added panning

--- render.py
+++ render.py
@@ -7,11 +7,13 @@ class Render:
         self.event = eventmanager
         self.event.register("new_planet", self.add_planet)
         self.event.register("mouse_down", self.mouse_down)
+        self.event.register("mouse_move", self.mouse_move)
         self.window = window
         self.system_radius = 0
-        self.scale_factor = 1
+        self.scale_factor = 1.0
         self.view = "space"
         self.planets = []
+        self.offset = (0,0)
     
     def add_planet(self,planet):
        self.planets.append(planet)
@@ -23,18 +25,30 @@ class Render:
         if button == 4:
             self.scale_factor *= 2
     
+    def mouse_move(self,pos,rel,buttons):
+        if buttons[0]:
+            self.offset = (self.offset[0]+rel[0]/self.scale_factor, self.offset[1]+rel[1]/self.scale_factor)
+    
     def draw(self):
         self.window.fill(pygame.Color("black"))
         w = self.window.get_width()
         h = self.window.get_height()
         scale = self.scale_factor*(min(w,h)*0.45)/self.system_radius
-        pygame.draw.circle(self.window,pygame.Color("white"),(w/2,h/2),int(0.3*scale))
+        centre = (int(w/2 + self.offset[0]*self.scale_factor),
+                  int(h/2 + self.offset[1]*self.scale_factor))
+        pygame.draw.circle(self.window,pygame.Color("yellow"),centre,int(0.3*scale))
         if self.view == "space":
             for p in self.planets:
                 r = int(math.ceil(p.orbit_radius*scale))
-                x = int(math.ceil(w/2 + r*math.cos(p.orbit_phase)))
-                y = int(math.ceil(h/2 + r*math.sin(p.orbit_phase)))
+                x = int(math.ceil(centre[0] + r*math.cos(p.orbit_phase)))
+                y = int(math.ceil(centre[1] + r*math.sin(p.orbit_phase)))
                 if p.planet_type != "dwarf planet":
-                    pygame.draw.circle(self.window,pygame.Color("white"),(w/2,h/2),r,1)
-                pygame.draw.circle(self.window,pygame.Color("white"),(x,y),int(self.earth_rad*p.planet_radius*scale))
+                    pygame.draw.circle(self.window,pygame.Color("white"),centre,r,1)
+                if p.planet_type == "dwarf planet":
+                    c = pygame.Color("green")
+                if p.planet_type == "terrestrial planet":
+                    c = pygame.Color("blue")
+                if p.planet_type == "gas giant":
+                    c = pygame.Color("red")
+                pygame.draw.circle(self.window,c,(x,y),int(self.earth_rad*p.planet_radius*scale))
             
